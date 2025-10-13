24608f950846f86b412df776685cfff8bd858e8f
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-10-13 22:53:45 +0530

    new world added

---
+++ code_java/Codes/src/com/demo/pro/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.demo.pro;
+
+public class HelloWorld {
+	
+	public void heloWorldDemo() {
+		System.out.println("Hello Java World ");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HelloWorld a=new HelloWorld();
+		a.heloWorldDemo();
+
+	}
+
+}

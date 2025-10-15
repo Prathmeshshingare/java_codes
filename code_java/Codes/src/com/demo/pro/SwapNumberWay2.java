a26f0e0889fb5e6e034f932ba3054d10bc8e5ddf
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-10-15 13:25:00 +0530

    modification of code swap1

---
+++ code_java/Codes/src/com/demo/pro/SwapNumberWay2.java
@@ -0,0 +1,14 @@
+package com.demo.pro;
+
+public class SwapNumberWay2 {
+	
+	void swapNumberWay2(int a,int b) {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
